581010c02bb395ad7ec6828b12d07791b2916873
yukito1225 <64147643+yukito1225@users.noreply.github.com>
2021-09-28 15:32:36 +0900

    Add files via upload

---
+++ Script/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float speed;
+    int count;
+    public Text countText;
+    AudioSource getSE;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        getSE = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var dir = Vector3.zero;
+
+        //ターゲット端末の縦横をの表示に合わせてremapする
+        dir.x = Input.acceleration.x;
+        dir.y = Input.acceleration.y;
+
+        //clamp acceleration vector to the unit sphere
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        dir *= Time.deltaTime;
+
+        transform.Translate(dir * speed);
+
+        var rotRH = Input.gyro.attitude;
+        var rot = new Quaternion(-rotRH.x, -rotRH.z, -rotRH.y, rotRH.w) * Quaternion.Euler(90f, 0f, 0f);
+
+        transform.localRotation = rot;
+
+
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveH, 0, moveV);
+        rb.AddForce(move * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            getSE.Play();
+        }
+        else if (other.gameObject.CompareTag("Bottom"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+     }
+
+    void SetCountText()
+    {
+        countText.text = "ゲット数:" + count.ToString();
+    }
+}
